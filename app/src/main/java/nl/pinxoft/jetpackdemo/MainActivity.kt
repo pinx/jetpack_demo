a7cb5ee51b85ec96348bcc9a5ebb435764b63993
pinx <marcel.van.pinxteren@gmail.com>
2020-10-05 15:48:26 +0200

    Initial commit with updated Gradle (6.6.1) and Kotlin (1.4.10)

---
+++ app/src/main/java/nl/pinxoft/jetpackdemo/MainActivity.kt
@@ -0,0 +1,38 @@
+package nl.pinxoft.jetpackdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import nl.pinxoft.jetpackdemo.ui.JetpackDemoTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackDemoTheme {
+        Greeting("Android")
+    }
+}
